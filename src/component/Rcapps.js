5786715670dbd8866c5d869c2fbb853aca3e57d1
ls <wjshku@connect.hku.hk>
2020-01-03 16:12:05 +0800

    footer

--- src/component/Rcapps.js
+++ src/component/Rcapps.js
@@ -5,8 +5,9 @@ import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
 import { makeStyles } from '@material-ui/core/styles';
 import Rclogo from "./Logo.png"
-import { Divider } from '@material-ui/core';
+import { Divider, List } from '@material-ui/core';
 import Container from '@material-ui/core/Container';
+import ListItem from '@material-ui/core/ListItem';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -40,33 +41,50 @@ export default function Menu(){
           <Tab></Tab>
             </Tabs>
             <Divider variant='middle'/>
+            <h2 style={{textAlign:'center',color:'white'}}>our team</h2>
             <div style={{ width: '100%' }}>
-            <Container maxWidth="md">
+            <Container maxWidth="lg">
             <Box 
         display="flex"
         flexWrap="wrap"
-        flexShrink={1} 
+        justifyContent="space-around"
       >
-    <Box >
+    <Box  flexShrink={1} >
           <img src={Rclogo}  width="250px"></img>
           <p>this is ...</p>
         </Box>
-        <Box >
+        <Box flexShrink={1} >
           <img src={Rclogo}  width="250px"></img>
           <p>this is ...</p>
         </Box>
-        <Box >
+        <Box flexShrink={1} >
           <img src={Rclogo}  width="250px"></img>
           <p>this is ...</p>
         </Box>
-        <Box >
+        <Box flexShrink={1} >
           <img src={Rclogo}  width="250px"></img>
           <p>this is ...</p>
         </Box>
       </Box>
       </Container>
     </div>
-     
+    <Divider variant='middle'/>
+    <div>
+      <Box display="flex" justifyContent="space-around">
+        <List>
+          <ListItem component="a" href="https://material-ui.com/zh/api/list-item/#import" divider="true"><font size="5" color="white">event1</font></ListItem>
+          <ListItem >subevent</ListItem>
+        </List>
+        <List>
+        <ListItem component="a" href="https://material-ui.com/zh/api/list-item/#import" divider="true"><font size="5" color="white">event2</font></ListItem>
+        <ListItem >subevent</ListItem>
+        </List>
+        <List>
+        <ListItem component="a" href="https://material-ui.com/zh/api/list-item/#import" divider="true"><font size="5" color="white">event3</font></ListItem>
+        <ListItem >subevent</ListItem>
+        </List>
+        </Box>
+      </div>
         </div>
     )
 }
